70a8d35be00f58457919c072c26129055e29bc34
aoba <c0b210910f@edu.teu.ac.jp>
2022-10-18 14:19:22 +0900

    練習問題03-3

--- ex03/maze.py
+++ ex03/maze.py
@@ -7,4 +7,8 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack() #練習問題２
 
+    tori = tk.PhotoImage(file="fig/1.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag="tori") #練習問題３
+
     root.mainloop()
