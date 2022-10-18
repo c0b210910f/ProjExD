135f437b95e26bb8cd55083dacdb7314b67c58e1
aoba <c0b210910f@edu.teu.ac.jp>
2022-10-18 14:14:50 +0900

    練習問題03-2

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,6 +2,9 @@ import tkinter as tk
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("迷えるこうかとん")#練習問題１
+
+    canv = tk.Canvas(root, width=1500, height=900, bg="black")
+    canv.pack() #練習問題２
 
     root.mainloop()
