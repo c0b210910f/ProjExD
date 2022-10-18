fb040973747a52d795fe566db555c118a4aca35f
aoba <c0b210910f@edu.teu.ac.jp>
2022-10-18 14:42:28 +0900

    練習問題03-7

--- ex03/maze.py
+++ ex03/maze.py
@@ -8,6 +8,23 @@ def key_up(event):
     global key
     key = ""
 
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+
+    if key == "Down":
+        cy += 20
+
+    if key == "Left":
+        cx -= 20
+
+    if key == "Right":
+        cx += 20
+
+    canv.coords("tori", cx, cy)
+    root.after(100, main_proc)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -26,6 +43,7 @@ if __name__ == "__main__":
 
     root.bind("<KeyRelease>", key_up) #練習問題６
 
+    main_proc() #練習問題７
 
 
     root.mainloop()
