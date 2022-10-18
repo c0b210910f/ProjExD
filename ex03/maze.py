c86d4206a7d188f2c375cce0fd9d4838197d9b16
aoba <c0b210910f@edu.teu.ac.jp>
2022-10-18 17:16:29 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import maze_maker as mm #練習問題８
+import tkinter.messagebox as tkm
+import sys
 
 def key_down(event):
     global key
@@ -12,6 +14,7 @@ def key_up(event):
 def main_proc():
     global mx, my
     global cx, cy
+    global jid
     if key == "Up":
         my -= 1
 
@@ -24,7 +27,7 @@ def main_proc():
     if key == "Right":
         mx += 1
 
-    if maze_lst[my][mx] == 0:
+    if maze_lst[my][mx] != 1:
         cx, cy = mx*100+50, my*100+50 #練習問題１１
     else:
         if key == "Up":
@@ -39,9 +42,22 @@ def main_proc():
         if key == "Right":
             mx -= 1
 
-
     canv.coords("tori", cx, cy)
+    if maze_lst[my][mx] == maze_lst[7][14]:
+        root.after_cancel(jid)
+        tkm.showinfo("ゴール！", "ゴール！おめでとう！！")
+        sys.exit()
+    if maze_lst[my][mx] == 4:
+        tkm.showwarning("ゲームオーバー","ゲームオーバー！！また挑戦してね！")
+        sys.exit()
+
     root.after(100, main_proc)
+
+def count_up():
+    global tmr, jid
+    tmr += 1
+    label["text"] = tmr
+    jid = root.after(1000, count_up)
 
 
 if __name__ == "__main__":
@@ -52,7 +68,7 @@ if __name__ == "__main__":
     canv.pack() #練習問題２
 
     tori = tk.PhotoImage(file="fig/1.png")
-    mx, my = 1, 1
+    mx, my = 0, 1
     cx, cy = 300, 400
     #canv.create_image(cx, cy, image=tori, tag="tori") #練習問題３
 
@@ -69,8 +85,20 @@ if __name__ == "__main__":
     mm.show_maze(canv, maze_lst) #練習問題１０
 
     main_proc() #練習問題７
+   
 
     canv.create_image(cx, cy, image=tori, tag="tori") #こうかとんを迷路の上に
+
+    label = tk.Label(root, font=("", 80))
+    label.pack()
+
+    tmr = 0
+    jid = None
+    root.after(1000, count_up)
+
+    #root.bind("<KeyPress>", key_down)
+
+        
 
 
     root.mainloop()
