b2add6549e323200e72ef8f2f98c17fc593b6318
aoba <c0b210910f@edu.teu.ac.jp>
2022-10-25 15:42:49 +0900

    3限基本機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,6 +2,15 @@ import pygame as pg
 import sys
 from random import randint
 
+def check_bound(obj_rct, scr_rct):
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate =  -1
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
@@ -18,8 +27,10 @@ def main():
     bomb_sfc = pg.Surface((20,20))
     bomb_sfc.set_colorkey((0, 0, 0))
     pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
-    bomb_rct = tori_sfc.get_rect()
+    bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx, bomb_rct.centery = randint(0, scrn_rct.width), randint(0, scrn_rct.height)
+
+    vx, vy = +1, +1
 
     clock = pg.time.Clock()
 
@@ -41,8 +52,28 @@ def main():
         if key_states[pg.K_RIGHT]:
             tori_rct.centerx += 1
         
+        yoko, tate = check_bound(tori_rct, scrn_rct)
+        if yoko == -1:
+            if key_states[pg.K_LEFT]:
+                tori_rct.centerx += 1
+            if key_states[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+        if tate == -1:
+            if key_states[pg.K_UP]:
+                tori_rct.centery += 1
+            if key_states[pg.K_DOWN]:
+                tori_rct.centery -= 1
         scrn_sfc.blit(tori_sfc, tori_rct)#練習問題３
+
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *=yoko
+        vy *=tate
+
+        bomb_rct.move_ip(vx, vy)
         scrn_sfc.blit(bomb_sfc, bomb_rct)#練習問題５
+
+        if tori_rct.colliderect(bomb_rct):
+            return
         
         pg.display.update()
         clock.tick(1000)
